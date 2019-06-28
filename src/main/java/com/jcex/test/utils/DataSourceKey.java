620cf5ecc3a113b15a7582d9d2e2c7b5404e4ca2
xiaocain <1768366415@qq.com>
2019-06-28 22:16:01 +0800

    测试

--- src/main/java/com/jcex/test/utils/DataSourceKey.java
+++
@@ -1,8 +0,0 @@
-package com.jcex.test.utils;
-
-public class DataSourceKey {
-
-    public static final String DATA_SOURCE_1 = "DATA_SOURCE_1";
-    public static final String DATA_SOURCE_2 = "DATA_SOURCE_2";
-
-}
